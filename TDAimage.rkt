6cbef5e5d59165e3b6af2f626c9075dae14aa84c
Klefur <joaquinalberto55@gmail.com>
2022-08-23 22:14:00 -0400

    se agregan comentarios simples y funciones de pertenencia

--- TDAimage.rkt
+++ TDAimage.rkt
@@ -1,12 +1,38 @@
 #lang racket
 
+;llamado de tda pixeles
+
 (require "TDApixels.rkt")
 
+; constructor
+
 (define (image x y . p)
-  (if (< (length p) (* x y))
-      p
+  (if (<= (length p) (* x y))
+      (cons x (cons y p))
       null))
 
-(define (pixbit-d x y bit d) (list x y bit d))
+; pertenencia
 
-(display (image 2 2 (pixbit-d 0 0 1 10)(pixbit-d 0 1 1 10)(pixbit-d 1 0 1 10)(pixbit-d 1 1 1 10)))
+(define (bitmap? imagen)
+  (if (= (length (cddr imagen)) (* (car imagen) (cadr imagen)))
+      (if (map pixbit-d? (caddr imagen))
+          (map pixbit-d? (caddr imagen))
+          #f)
+      #f))
+
+(define (pixmap? imagen)
+  (if (= (length (cddr imagen)) (* (car imagen) (cadr imagen)))
+      (if (map pixrgb-d? (caddr imagen))
+          (caddr imagen)
+          #f)
+      #f))
+
+(define (hexmap? imagen)
+  (if (= (length (cddr imagen)) (* (car imagen) (cadr imagen)))
+      (if (pixhex-d? (caddr imagen))
+          (caddr imagen)
+          #f)
+      #f))
+
+(bitmap? (image 2 2 (pixbit-d 0 0 1 10)(pixbit-d 0 1 1 10)(pixbit-d 1 0 1 10)(pixbit-d 1 1 1 10)))
+(bitmap? (image 2 2 (pixbit-d 0 0 5 10)(pixbit-d 0 1 5 10)(pixbit-d 1 0 5 10)(pixbit-d 1 1 5 10)))
--- TDApixels.rkt
+++ TDApixels.rkt
@@ -1,7 +1,46 @@
 #lang racket
-  
+
+; constructores
+
 (define (pixbit-d x y bit d) (list x y bit d))
 
 (define (pixrgb-d x y r g b d) (list x y r g b d))
 
 (define (pixhex-d x y hex d) (list x y hex d))
+
+; pertenencia
+
+(define (pixbit-d? pixel)
+  (if (list? pixel)
+      (if (= (length pixel) 4)
+          (if (or (= (caddr pixel) 1) (= (caddr pixel) 0))
+              #t
+              #f)
+          #f)
+      #f))
+
+(define (pixrgb-d? pixel)
+  (if (list? pixel)
+      (if (= (length pixel) 6)
+          (if (and (>= (caddr pixel) 0) (<= (caddr pixel) 255)
+               (>= (cadddr pixel) 0) (<= (cadddr pixel) 255)
+               (>= (car (cddddr pixel)) 0) (<= (car (cddddr pixel)) 255))
+              #t
+              #f)
+          #f)
+      #f))
+
+(define (pixhex-d? pixel)
+  (if (list? pixel)
+      (if (= (length pixel) 4)
+          (if (string? (caddr pixel))
+              (if (and (= (car pixel) "#") (= (length pixel) 7))
+                  #t
+                  #f)
+              #f)
+          #f)
+      #f))
+
+;exportacion de funciones para su posterior uso
+
+(provide (all-defined-out))

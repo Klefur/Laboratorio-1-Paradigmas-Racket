c6f859b8cbbfbfa4d33ee4e6956311d8a2736ece
Klefur <joaquinalberto55@gmail.com>
2022-09-11 20:31:56 -0300

    se agrega funcion edit y derivados

--- TDAPixel.rkt
+++ TDAPixel.rkt
@@ -1,15 +1,36 @@
 #lang racket
+;-----------------------------------TDA PIXEL-------------------------------------------------------------------
 
-; constructores
+;-----------------------------------REPRESENTACION--------------------------------------------------------------
 
+; Este TDA corresponde a un pixel y dada su similitud los selectores y modificadores seran iguales en su mayoria.
+; Se guarda en una lista la posicion en x, y, su color y su profundidad
+; pixrgb (int X int X lista X int)
+; pixbit (int X int X int X int)
+; pixhex (int X int X string X int)
+;-----------------------------------CONSTRUCTORES---------------------------------------------------------------
+
+;Dom: int X int X int X int
+;Rec: lista del tipo pixbit-d
+;Descripcion: Se crea un pixel con color bit
 (define (pixbit-d x y bit d) (list x y bit d))
 
+;Dom: int X int X int X int X int X int
+;Rec: lista del tipo pixrgb-d
+;     (int X int X lista X int)
+;Descripcion: Se crea un pixel con color RGB
 (define (pixrgb-d x y r g b d) (list x y (list r g b) d))
 
+;Dom: int X int X string X int
+;Rec: lista del tipo pixhex-d
+;Descripcion: Se crea un pixel con color hexagesimal
 (define (pixhex-d x y hex d) (list x y hex d))
 
-; pertenencia
+;-----------------------------------PERTENENCIA----------------------------------------------------------------
 
+;Dom: lista del tipo pixel
+;Rec: Boolean
+;Descripcion: Se verifica si el pixel es pixbit-d
 (define (pixbit-d? pixel)
   (if (list? pixel)
       (if (= (length pixel) 4)
@@ -19,6 +40,9 @@
           #f)
       #f))
 
+;Dom: lista del tipo pixel
+;Rec: Boolean
+;Descripcion: Se verifica si el pixel es pixrgb-d
 (define (pixrgb-d? pixel)
   (if (list? pixel)
       (if (= (length pixel) 4)
@@ -28,6 +52,9 @@
           #f)
       #f))
 
+;Dom: lista del tipo pixel
+;Rec: Boolean
+;Descripcion: Se verifica si el pixel es pixhex-d
 (define (pixhex-d? pixel)
   (if (list? pixel)
       (if (= (length pixel) 4)
@@ -37,51 +64,139 @@
           #f)
       #f))
 
-;selectores
+;-----------------------------------SELECTORES-----------------------------------------------------------------
 
+;Dom: lista del tipo pixel [bit | hex | rgb]
+;Rec: int
+;Descripcion: entrega la posicion en x del pixel
 (define (getPosX pixel)
   (if (or (pixhex-d? pixel) (pixbit-d? pixel) (pixrgb-d? pixel))
       (car pixel)
       null))
 
+;Dom: lista del tipo pixel [bit | hex | rgb]
+;Rec: int
+;Descripcion: entrega la posicion en y del pixel
 (define (getPosY pixel)
   (if (or (pixhex-d? pixel) (pixbit-d? pixel) (pixrgb-d? pixel))
       (cadr pixel)
       null))
 
+;Dom: lista del tipo pixel [bit | hex | rgb]
+;Rec: [int | string | lista]
+;Descripcion: entrega el color del pixel
 (define (getColor pixel)
   (if (or (pixhex-d? pixel) (pixbit-d? pixel) (pixrgb-d? pixel))
       (caddr pixel)
       null))
 
+;Dom: lista del tipo pixel [bit | hex | rgb]
+;Rec: int
+;Descripcion: entrega la profundidad del pixel
 (define (getDepth pixel)
   (if (or (pixhex-d? pixel) (pixbit-d? pixel) (pixrgb-d? pixel))
       (cadddr pixel)
       null))
 
-; modificadores
+;Dom: lista del tipo pixrgb-d 
+;Rec: int
+;Descripcion: entrega el color rojo de un pixrgb-d
+(define (getR pixel)
+  (if (pixrgb-d? pixel)
+      (car (getColor pixel))
+      null))
 
+;Dom: lista del tipo pixrgb-d
+;Rec: int
+;Descripcion: entrega el color verde de un pixrgb-d
+(define (getG pixel)
+  (if (pixrgb-d? pixel)
+      (cadr (getColor pixel))
+      null))
+
+;Dom: lista del tipo pixrgb-d
+;Rec: int
+;Descripcion: entrega el color azul de un pixrgb-d
+(define (getB pixel)
+  (if (pixrgb-d? pixel)
+      (caddr (getColor pixel))
+      null))
+
+;-----------------------------------MODIFICADORES--------------------------------------------------------------
+
+;Dom: lista del tipo pixel [bit | hex | rgb]
+;Rec: lista del tipo pixel [bit | hex | rgb]
+;Descripcion: modifica la posicion en x del pixel
 (define (setPosX pixel x)
   (cond
     [(pixbit-d? pixel) (pixbit-d x (getPosY pixel) (getColor pixel) (getDepth pixel))]
     [(pixrgb-d? pixel) (pixrgb-d x (getPosY pixel) (car (getColor pixel)) (cadr (getColor pixel)) (caddr (getColor pixel)) (getDepth pixel))]
     [(pixhex-d? pixel) (pixhex-d x (getPosY pixel) (getColor pixel) (getDepth pixel))]
-    [else pixel]))
+    [else null]))
 
+;Dom: lista del tipo pixel [bit | hex | rgb]
+;Rec: lista del tipo pixel [bit | hex | rgb]
+;Descripcion: modifica la posicion en y del pixel
 (define (setPosY pixel y)
   (cond
     [(pixbit-d? pixel) (pixbit-d (getPosX pixel) y (getColor pixel) (getDepth pixel))]
     [(pixrgb-d? pixel) (pixrgb-d (getPosX pixel) y (car (getColor pixel)) (cadr (getColor pixel)) (caddr (getColor pixel)) (getDepth pixel))]
     [(pixhex-d? pixel) (pixhex-d (getPosX pixel) y (getColor pixel) (getDepth pixel))]
-    [else pixel]))
+    [else null]))
 
+;Dom: lista del tipo pixbit-d
+;Rec: lista del tipo pixbit-d
+;Descripcion: modifica el color de un pixrgb-d
+(define (setBit pixel bit)
+  (if (and (pixbit-d? pixel) (or (= bit 1) (= bit 0)))
+      (pixbit-d (getPosX pixel) bit (getDepth pixel))
+      pixel))
+
+;Dom: lista del tipo pixrgb-d
+;Rec: lista del tipo pixrgb-d
+;Descripcion: modifica el color rojo de un pixrgb-d
+(define (setR pixel R)
+  (if (and (pixrgb-d? pixel) (or (<= R 255) (>= R 0)))
+      (pixrgb-d (getPosX pixel) (getPosY pixel) R (getG pixel) (getB pixel) (getDepth pixel))
+      pixel))
+
+;Dom: lista del tipo pixrgb-d
+;Rec: lista del tipo pixrgb-d
+;Descripcion: modifica el color verde de un pixrgb-d
+(define (setG pixel G)
+  (if (and (pixrgb-d? pixel) (or (<= G 255) (>= G 0)))
+      (pixrgb-d (getPosX pixel) (getPosY pixel) (getR pixel) G (getB pixel) (getDepth pixel))
+      pixel))
+
+;Dom: lista del tipo pixrgb-d
+;Rec: lista del tipo pixrgb-d
+;Descripcion: modifica el color azul de un pixrgb-d
+(define (setB pixel B)
+  (if (and (pixrgb-d? pixel) (or (<= B 255) (>= B 0)))
+      (pixrgb-d (getPosX pixel) (getPosY pixel) (getR pixel) (getG pixel) B (getDepth pixel))
+      pixel))
+
+;Dom: lista del tipo pixhex-d
+;Rec: lista del tipo pixhex-d
+;Descripcion: modifica el color de un pixhex-d
+(define (setHex pixel hex)
+  (if (pixhex-d? pixel)
+      (pixhex-d (getPosX pixel) (getPosY pixel) hex (getDepth pixel))
+      pixel))
+
+;-----------------------------------OTRAS FUNCIONES------------------------------------------------------------
+
+;Dom: lista del tipo pixrgb-d
+;Rec: lista del tipo pixhex-d
+;Descripcion: convierte un pixrgb-d a pixhex-d
 (define (pixrgb->pixhex pixel)
   (if (pixrgb-d? pixel)
       (pixhex-d (getPosX pixel) (getPosY pixel) (string-append "#" (car (rgb->hex pixel)) (cadr (rgb->hex pixel)) (caddr (rgb->hex pixel))) (getDepth pixel))
-      pixel))
+      null))
 
-; otras funciones
-
+;Dom: int
+;Rec: string
+;Descripcion: transforma un entero a formato hexagesimal
 (define (rgb->stringHex numero)
   (cond
     [(= numero 10) "A"]
@@ -92,6 +207,9 @@
     [(= numero 15) "F"]
     [else (number->string numero)]))
 
+;Dom: lista del pixrgb-d
+;Rec: string
+;Descripcion: transforma el color rgb a color hex
 (define (rgb->hex pixel)
   (map (lambda (c)
          (if (= (remainder c 16) 0)
--- TDAimage.rkt
+++ TDAimage.rkt
@@ -3,9 +3,19 @@
 ;llamado de tda pixeles
 
 (require "TDAPixel.rkt")
+;-----------------------------------TDA IMAGE-------------------------------------------------------------------
 
-; constructor
+;-----------------------------------REPRESENTACION--------------------------------------------------------------
 
+; Este TDA corresponde a una imagen.
+; Se guarda en una lista el ancho, el largo, una lista de pixeles [pixbit-d | pixrgb-d | pixhex-d] y un color para
+; definir que fue comprimido
+; (int X int X [pixbit-d | pixrgb-d | pixhex-d] X color [int | lista | string])
+;-----------------------------------CONSTRUCTORES---------------------------------------------------------------
+
+;Dom: ancho x (int), largo y (int), pixeles p (pixbit-d | pixrgb-d | pixhex-d)
+;Rec: lista image
+;Descripcion: Se crea una imagen
 (define (image x y . p)
   (if (<= (length p) (* x y))
       (if (null? p)
@@ -13,76 +23,124 @@
           (list x y p))
       null))
 
-; pertenencia
+;-----------------------------------PERTENENCIA-----------------------------------------------------------------
 
+;Dom: lista del tipo image
+;Rec: Boolean
+;Descripcion: Se verifica si la imagen esta compuesta de pixeles del tipo pixbit-d
 (define (bitmap? imagen)
   (if (= (length (caddr imagen)) (* (car imagen) (cadr imagen)))
       (pixbit-d? (car (caddr imagen)))
       #f))
 
+;Dom: lista del tipo image
+;Rec: Boolean
+;Descripcion: Se verifica si la imagen esta compuesta de pixeles del tipo pixrgb-d
 (define (pixmap? imagen)
   (if (= (length (caddr imagen)) (* (car imagen) (cadr imagen)))
       (pixrgb-d? (car (caddr imagen)))
       #f))
 
+;Dom: lista del tipo image
+;Rec: Boolean
+;Descripcion: Se verifica si la imagen esta compuesta de pixeles del tipo pixhex-d
 (define (hexmap? imagen)
   (if (= (length (caddr imagen)) (* (car imagen) (cadr imagen)))
       (pixhex-d? (car (caddr imagen)))
       #f))
 
+;Dom: lista del tipo image
+;Rec: Boolean
+;Descripcion: Se verifica si la imagen esta comprimida
 (define (compressed? imagen)
-  (if (= (length (caddr imagen)) (* (car imagen) (cadr imagen)))
-      #f
-      #t))
+  (= (length (imagen)) 4))
 
+;Dom: lista del tipo image
+;Rec: Boolean
+;Descripcion: Se verifica si la imagen tiene un formato de pixeles valido
 (define (image? imagen)
   (or (pixmap? imagen) (bitmap? imagen) (hexmap? imagen)))
  
-; selectores
+;-----------------------------------SELECTORES-----------------------------------------------------------------
 
+;Dom: lista del tipo image
+;Rec: int
+;Descripcion: entrega el ancho de la imagen
 (define (getLenX imagen)
   (if (image? imagen)
       (car imagen)
       null))
 
+;Dom: lista del tipo image
+;Rec: int
+;Descripcion: entrega el largo de la imagen
 (define (getLenY imagen)
   (if (image? imagen)
       (cadr imagen)
       null))
 
+;Dom: lista del tipo image
+;Rec: lista [pixbit-d | pixrgb-d | pixhex-d]
+;Descripcion: entrega los pixeles que componen la imagen
 (define (getPixeles imagen)
   (if (image? imagen)
       (caddr imagen)
       null))
 
+;Dom: lista de pixeles
+;Rec: [pixbit-d | pixrgb-d | pixhex-d]
+;Descripcion: entrega el primer pixel de una lista de pixeles
 (define (firstPix pixeles)
   (if (list? pixeles)
       (car pixeles)
       null))
-
+;Dom: lista de pixeles
+;Rec: [pixbit-d | pixrgb-d | pixhex-d]
+;Descripcion: entrega el resto pixeles de una lista de pixeles
 (define (nextPix pixeles)
   (if (list? pixeles)
       (cdr pixeles)
       null))
 
-; modificadores
+;-----------------------------------MODIFICADORES--------------------------------------------------------------
 
+;Dom: lista del tipo image
+;Rec: lista del tipo image
+;Descripcion: modifica los pixeles de una imagen
 (define (setPixeles imagen . p)
-  (append imagen p))
+  (if (= (length imagen) 2)
+      (append imagen p)
+      (append (image (getLenX imagen) (getLenY imagen)) p)))
 
+;Dom: lista del tipo image
+;Rec: lista del tipo image
+;Descripcion: modifica el ancho de una imagen
 (define (setLenX imagen x)
   (if (image? imagen)
       (setPixeles (image x (getLenY imagen)) (getPixeles imagen))
       null))
 
+;Dom: lista del tipo image
+;Rec: lista del tipo image
+;Descripcion: modifica el largo de una imagen
 (define (setLenY imagen y)
   (if (image? imagen)
       (setPixeles (image (getLenX imagen) y) (getPixeles imagen))
       null))
 
+;Dom: lista del tipo image
+;Rec: lista del tipo image
+;Descripcion: establece el valor del color que fue comprimido
 (define (setCompressV imagen . color)
   (append imagen color))
 
+;-----------------------------------OTRAS FUNCIONES------------------------------------------------------------
+
+;Dom: lista del tipo image
+;Rec: lista del tipo image
+;Descripcion: modifica los pixeles de una imagen volteandola horizontalmente
+;Recursion: natural
+;Justificacion: permite ir recorrer la lista de pixeles y a su vez ir modificandolos para contruir la imagen volteada
 (define (flipH imagen)
   (define (lambda1 pixeles)
      (if (null? pixeles)
@@ -92,6 +150,11 @@
       (setPixeles imagen (lambda1 (getPixeles imagen)))
       null))
 
+;Dom: lista del tipo image
+;Rec: lista del tipo image
+;Descripcion: modifica los pixeles de una imagen volteandola verticalmente
+;Recursion: natural
+;Justificacion: permite ir recorrer la lista de pixeles y a su vez ir modificandolos para contruir la imagen volteada
 (define (flipV imagen)
   (define (lambda1 pixeles)
      (if (null? pixeles)
@@ -100,7 +163,13 @@
   (if (image? imagen)
       (setPixeles imagen (lambda1 (getPixeles imagen)))
       null))
-  
+
+;Dom: image X int X int X int X int
+;Rec: lista del tipo image
+;Descripcion: modifica los pixeles de una imagen recortandola de acuerdo a parametros establecidos, reescalando la imagen
+;en tamaño y cambiando la posicion de los pixeles
+;Recursion: natural
+;Justificacion: permite ir recorrer la lista de pixeles y a su vez ir modificandolos para contruir la imagen recortada
 (define (crop imagen x0 y0 x1 y1)
   (define (lambda1 pixeles)
      (if (null? pixeles)
@@ -110,14 +179,22 @@
              (cons (setPosY (setPosX (firstPix pixeles) (- (getPosX (firstPix pixeles)) x0)) (- (getPosY (firstPix pixeles)) y0)) (lambda1 (nextPix pixeles)))
              (lambda1 (nextPix pixeles)))))
   (if (and (<= 0 x0) (>= (getLenX imagen) x1) (<= 0 y0) (>= (getLenX imagen) y1))
-      (append (image (+ (- x1 x0) 1) (+ (- y1 y0) 1)) (lambda1 (getPixeles imagen)))
+      (setPixeles (image (+ (- x1 x0) 1) (+ (- y1 y0) 1)) (lambda1 (getPixeles imagen)))
       imagen))
 
+;Dom: lista del tipo image con pixeles del tipo pixrgb-d
+;Rec: lista del tipo image con pixeles del tipo pixhex-d
+;Descripcion: transforma una imagen pixrgb-d a una imagen pixhex-d
 (define (imgRGB->imgHex imagen)
   (if (pixmap? imagen)
       (setPixeles imagen (map pixrgb->pixhex (getPixeles imagen)))
       imagen))
 
+;Dom: lista del tipo image
+;Rec: lista
+;Descripcion: crea una lista de los colores y la cantidad de veces que se repite en la imagen
+;Recursion: cola
+;Justificacion: facilita recorrer la lista de pixeles y a su vez cuando este vacia entregar directamente el histograma
 (define (histogram imagen)
   (define (histogramR pixeles histo)
     (if (null? pixeles)
@@ -127,50 +204,104 @@
       (histogramR (getPixeles imagen) (list))
       imagen))
 
+;Dom: lista del tipo image
+;Rec: lista del tipo image
+;Descripcion: rota una imagen en 90° en sentido horario
 (define (rotate90 imagen)
   (if (image? imagen)
-      (setPixeles (image (getLenY imagen) (getLenX imagen)) (map (lambda (p) (setPosX (setPosY p (getPosX p)) (- (- (getLenY imagen) 1) (getPosY p)))) (getPixeles imagen)))
+      (setPixeles imagen (map (lambda (p) (setPosX (setPosY p (getPosX p)) (- (- (getLenY imagen) 1) (getPosY p)))) (getPixeles imagen)))
       null))
 
+;Dom: lista del tipo image
+;Rec: lista del tipo image
+;Descripcion: comprime una imagen eliminando los pixeles mas repetidos
 (define (compress imagen)
   (setCompressV (setPixeles (image (getLenX imagen) (getLenY imagen))
   (filter (lambda (pixel) (not (equal? (car (getMayor (histogram imagen))) (getColor pixel)))) (getPixeles imagen)))
   (car (getMayor (histogram imagen)))))
 
-; otras funciones
+;Dom: lista del tipo image
+;Rec: lista del tipo image
+;Descripcion: se le entrega un funcion f para modificar una imagen
+(define (edit f imagen)
+  (setPixeles imagen (map f (getPixeles imagen))))
 
+;Dom: lista del tipo pixbit
+;Rec: lista del tipo pixbit
+;Descripcion: invierte los colores del pixel
+(define (invertColorBit pixbit)
+  (if (pixbit-d? pixbit)
+      (pixbit-d (getPosX pixbit) (getPosY pixbit) (- 1 (getColor pixbit)) (getDepth pixbit))
+      null))
+
+;Dom: lista del tipo pixrgb
+;Rec: lista del tipo pixrgb
+;Descripcion: invierte los colores del pixel
+(define (invertColorRGB pixrgb)
+  (if (pixrgb-d? pixrgb)
+      (pixrgb-d (getPosX pixrgb) (getPosY pixrgb) (- 255 (getR pixrgb)) (- 255 (getG pixrgb)) (- 255 (getB pixrgb)) (getDepth pixrgb))
+      null))
+
+;Dom: funcion x funcion x funcion x pixrgb
+;Rec: pixrgb
+;Descripcion: se elije un canal del pixel y se le introduce una funcion de operacion para ese canal modificando el color del pixel
+(define (adjustChannel f1 f2 f3) (lambda (p)
+  (f2 p (f3 (f1 p)))))
+
+;Dom: lista
+;Rec: lista
+;Descripcion: entrega el color mas repetido de la lista histograma
+;Recursion: cola
+;Justificacion: facilita el seguimiento del mayor entregandolo por parametro retornando devuelta cuando ya se haya recorrido todo el histograma 
 (define (getMayor histograma)
-  (define (mRepeat histograma mayor)
+  (define (getMR histograma mayor)
     (if (null? histograma)
         mayor
         (if (> (cadr (car histograma)) (cadr mayor))
             (mRepeat (cdr histograma) (car histograma))
             (mRepeat (cdr histograma) mayor))))
-  (mRepeat (cdr histograma) (car histograma)))
+  (getMR (cdr histograma) (car histograma)))
 
-(define (esta? pixel histograma)
+;Dom: lista
+;Rec: boolean
+;Descripcion: entrega un booleando que indica si esta un color dentro de la lista histograma
+;Recursion: cola
+;Justificacion: al no necesitar estados pendientes se recorre la lista hasta encontrar el color o caso contrario retornar falso
+(define (estaC? pixel histograma)
   (if (null? histograma)
       #f
       (if (equal? (getColor pixel) (car (car histograma)))
           #t
-          (esta? pixel (cdr histograma)))))
+          (estaC? pixel (cdr histograma)))))
 
+;Dom: lista
+;Rec: lista
+;Descripcion: la funcion agrega el color a la lista histograma en caso de no estar y si esta se agrega uno a donde corresponde
 (define (agregar pixel histograma)
-  (if (esta? pixel histograma)
+  (if (estaC? pixel histograma)
       (agregar1 (getColor pixel) histograma)
       (append histograma (list (list (getColor pixel) 1)))))
 
+;Dom: lista
+;Rec: lista
+;Descripcion: agrega 1 al color que corresponde
+;Recursion: natural
+;Justificacion: facilita la modificacion directa de alguno de los elementos de la lista ya que se trabaja uno a uno
 (define (agregar1 color histograma)
   (if (null? histograma)
       null
       (if (equal? color (car (car histograma)))
       (cons (list (car (car histograma)) (+ (cadr (car histograma)) 1)) (agregar1 color (cdr histograma)))
       (cons (car histograma) (agregar1 color (cdr histograma))))))
-  
+
+;Dom: int
+;Rec: int
+;Descripcion: agrega 1
+(define (incCh c) (+ c 1))
 
 ; img prueba
 
-(define img1 (image 2 2 (pixbit-d 0 0 1 10)(pixbit-d 1 0 0 10)(pixbit-d 0 1 1 10)(pixbit-d 1 1 1 10)))
+(define img1 (image 2 2 (pixbit-d 0 0 1 20)(pixbit-d 1 0 0 10)(pixbit-d 0 1 1 20)(pixbit-d 1 1 1 10)))
 
 (define img2 (image 2 2 (pixrgb-d 0 0 55 205 105 10)(pixrgb-d 1 0 5 5 5 10)
                     (pixrgb-d 0 1 5 5 5 10)(pixrgb-d 1 1 5 5 5 10)))
